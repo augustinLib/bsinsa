8a4568be51df9804c99de7ea976a525042046e8c
augustinLib <2bambitious@gmail.com>
2022-11-18 01:54:02 +0900

    Feat: Add argparser

--- crawling/get_url.py
+++ crawling/get_url.py
@@ -1,3 +1,4 @@
+from distutils.log import error
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -5,6 +6,7 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import time
 import argparse
+import random
 
 def define_argparse():
     p = argparse.ArgumentParser()
@@ -25,28 +27,35 @@ url = config.url
 options = webdriver.ChromeOptions()
 # 탭 간 이동 활성화
 options.add_argument("no-sandbox")
-options.add_argument("headless")
+# options.add_argument("headless")
 
 links = []
 with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
     driver.get(url)
     driver.implicitly_wait(5)
+    error_num = 0
     
 
     for page_num in range(1, config.pages):
         driver.execute_script(f"switchPage(document.f1,{page_num});")
+        driver.implicitly_wait(5)
         for item_num in range(1,91):
-            link= driver.find_element(
-            By.CSS_SELECTOR,
-            f"#searchList > li:nth-child({item_num}) > div.li_inner > div.article_info > p.list_info > a")
+            try:
+                link= driver.find_element(
+                By.CSS_SELECTOR,
+                f"#searchList > li:nth-child({item_num}) > div.li_inner > div.article_info > p.list_info > a")
             
-            temp_link = link.get_attribute("href")
-            links.append(temp_link)
+                temp_link = link.get_attribute("href")
+                links.append(temp_link)
+            except:
+                print(f"{page_num}page {item_num}th error!")
+                error_num += 1
+                continue
 
-        time.sleep(2)
+        time.sleep(random.randint(1,4))
             
         
 
-
+print(f"{error_num} items omitted!")
 link_df= pd.DataFrame(links)
 link_df.to_csv("./crawling/links/test_link.csv")
