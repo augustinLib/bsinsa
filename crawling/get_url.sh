6ad74f0b65fea35708894f5d7cb514bfb3c152a8
augustinLib <2bambitious@gmail.com>
2022-11-18 23:51:52 +0900

    Feat: Add get_url

---
+++ crawling/get_url.sh
@@ -0,0 +1,17 @@
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001011 --pages 17 --type top_sleeveless
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001001 --pages 401 --type top_short
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001005 --pages 302 --type top_sweatshirt
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001006 --pages 128 --type top_knit
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001004 --pages 198 --type top_hood
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001010 --pages 104 --type top_long
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001002 --pages 143 --type top_shirts
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/001003 --pages 38 --type top_polo
+
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003002 --pages 88 --type bottom_denim
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003007 --pages 82 --type bottom_cotton
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003008 --pages 55 --type bottom_slacks
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003004 --pages 173 --type bottom_jogger
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003010 --pages 6 --type bottom_jumpper
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003009 --pages 101 --type bottom_short
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003006 --pages 31 --type bottom_etc
+python ./crawling/get_url.py --url https://www.musinsa.com/categories/item/003005 --pages 4 --type bottom_leggings
