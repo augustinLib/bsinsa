3cf340b093aa1dcb14b3de6f3390d06b4d47b82f
augustinLib <2bambitious@gmail.com>
2022-12-29 05:56:26 +0900

    Fix: Fix url crawler

--- crawling/get_url.py
+++ crawling/get_url.py
@@ -27,7 +27,7 @@ url = config.url
 options = webdriver.ChromeOptions()
 # 탭 간 이동 활성화
 options.add_argument("no-sandbox")
-# options.add_argument("headless")
+options.add_argument("headless")
 
 links = []
 with webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options) as driver:
