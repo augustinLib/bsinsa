033a703ed8be763c94a3f37011c1a716fc35e2e1
orange-fritters <mindongss@snu.ac.kr>
2023-01-11 19:28:56 +0900

    #08 updated navigation bar

--- frontend/src/components/Buttons/Button.tsx
+++ frontend/src/components/Buttons/Button.tsx
@@ -1,16 +1,46 @@
 import React, { MouseEventHandler } from "react";
+import styled from "styled-components";
 
 export interface ButtonProp {
   text: string;
   type: "header" | "navigation";
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
+export const HeaderButton = styled.button<{ typeOf: "header" | "navigation" }>`
+  margin-left: 10px;
+  margin-right: 10px;
+  padding: ${(props) =>
+    props.typeOf === "header" ? "0px" : "20px 20px 15px 15px"};
+  position: relative;
+  width: ${(props) => (props.typeOf === "header" ? "fit-content" : "auto")};
+  height: 100%;
+
+  background: ${(props) => (props.typeOf === "header" ? "000000" : "#1c1f33")};
+  color: #ffffff;
+  // border-color: ${(props) => (props.typeOf === "header" ? "black" : "none")};
+  border: ${(props) => (props.typeOf === "header" ? "black" : "none")};
+  border-radius: 5px;
+
+  font-weight: 700;
+  font-style: bold;
+  font-size: 20px;
+  font-family: "Work Sans";
+
+  vertical-align: middle;
+  text-align: center;
+  text-transform: capitalize;
+
+  &:hover {
+    font-weight: 900;
+    font-style: italic;
+  }
+`;
 
 const Button = ({ text, type, onClick }: ButtonProp) => {
   return (
-    <button className={`Button_${type}`} onClick={onClick}>
-      <p className="button-text">{text}</p>
-    </button>
+    <HeaderButton onClick={onClick} typeOf={type}>
+      {text}
+    </HeaderButton>
   );
 };
 
